dac360237c824640f5272623030af8cd64e65d68
chan-hwi <gazebo5@naver.com>
2023-10-22 21:21:07 +0900

    feat: change layout

--- app/(pages)/(main)/page.tsx
+++ app/(pages)/(main)/page.tsx
@@ -16,9 +16,9 @@ function Home() {
 		<main className="flex flex-col">
 			<header className="w-full h-[300px] bg-slate-700 text-white flex flex-col justify-center px-4 py-8">
 				<div className="flex flex-col gap-2">
-					<h1 className="text-4xl font-bold">U-zokbo</h1>
+					<h1 className="text-4xl font-bold">로고</h1>
 					<p className="text-slate-300 text-sm font-medium">
-						쉽고 편하게 과목별/교수님별 공부 자료들을 업로드하고 열람하세요
+						쉽고 편하게 과목별 공부 자료들을 업로드하고 열람하세요
 					</p>
 				</div>
 				<Button className="w-fit mt-12 bg-green-foreground hover:bg-green-700/95 font-bold">
@@ -27,7 +27,7 @@ function Home() {
 			</header>
 			<article className="flex flex-col gap-2 p-4">
 				<div className="flex gap-4 items-center">
-					<Input placeholder="필요한 자료를 검색해보세요" className="" />
+					<Input placeholder="필요한 공부 자료를 검색해보세요" className="" />
 					<Button size="icon" variant="primary">
 						<SearchIcon />
 					</Button>
@@ -41,31 +41,31 @@ function Home() {
 						</CardHeader>
 						<CardContent className="flex flex-col">
 							<ContentListItem
-								title="데이터 사이언스 개론 퀴즈"
+								title="데이터 사이언스 개론 개념 정리"
 								downloadCount={12}
 								thumbsUpCount={24}
 								date="1분 전"
 							/>
 							<ContentListItem
-								title="응용선형대수학 기말고사"
+								title="응용선형대수학 노트"
 								downloadCount={15}
 								thumbsUpCount={31}
 								date="5분 전"
 							/>
 							<ContentListItem
-								title="확률과 랜덤 프로세스 퀴즈"
+								title="확률과 랜덤 프로세스"
 								downloadCount={18}
 								thumbsUpCount={25}
 								date="9분 전"
 							/>
 							<ContentListItem
-								title="고급 프로그래밍 중간고사"
+								title="고급 프로그래밍 예제"
 								downloadCount={18}
 								thumbsUpCount={34}
 								date="12분 전"
 							/>
 							<ContentListItem
-								title="시계열분석 과제"
+								title="시계열분석"
 								downloadCount={23}
 								thumbsUpCount={32}
 								date="22분 전"
@@ -78,31 +78,31 @@ function Home() {
 						</CardHeader>
 						<CardContent className="flex flex-col">
 							<ContentListItem
-								title="데이터 사이언스 개론 퀴즈"
+								title="데이터 사이언스 개론 개념 정리"
 								downloadCount={12}
 								thumbsUpCount={24}
 								date="1분 전"
 							/>
 							<ContentListItem
-								title="응용선형대수학 기말고사"
+								title="응용선형대수학 노트"
 								downloadCount={15}
 								thumbsUpCount={31}
 								date="5분 전"
 							/>
 							<ContentListItem
-								title="확률과 랜덤 프로세스 퀴즈"
+								title="확률과 랜덤 프로세스"
 								downloadCount={18}
 								thumbsUpCount={25}
 								date="9분 전"
 							/>
 							<ContentListItem
-								title="고급 프로그래밍 중간고사"
+								title="고급 프로그래밍 예제"
 								downloadCount={18}
 								thumbsUpCount={34}
 								date="12분 전"
 							/>
 							<ContentListItem
-								title="시계열분석 과제"
+								title="시계열분석"
 								downloadCount={23}
 								thumbsUpCount={32}
 								date="22분 전"
@@ -115,31 +115,31 @@ function Home() {
 						</CardHeader>
 						<CardContent className="flex flex-col">
 							<ContentListItem
-								title="데이터 사이언스 개론 퀴즈"
+								title="데이터 사이언스 개론 개념 정리"
 								downloadCount={12}
 								thumbsUpCount={24}
 								date="1분 전"
 							/>
 							<ContentListItem
-								title="응용선형대수학 기말고사"
+								title="응용선형대수학 노트"
 								downloadCount={15}
 								thumbsUpCount={31}
 								date="5분 전"
 							/>
 							<ContentListItem
-								title="확률과 랜덤 프로세스 퀴즈"
+								title="확률과 랜덤 프로세스"
 								downloadCount={18}
 								thumbsUpCount={25}
 								date="9분 전"
 							/>
 							<ContentListItem
-								title="고급 프로그래밍 중간고사"
+								title="고급 프로그래밍 예제"
 								downloadCount={18}
 								thumbsUpCount={34}
 								date="12분 전"
 							/>
 							<ContentListItem
-								title="시계열분석 과제"
+								title="시계열분석"
 								downloadCount={23}
 								thumbsUpCount={32}
 								date="22분 전"
