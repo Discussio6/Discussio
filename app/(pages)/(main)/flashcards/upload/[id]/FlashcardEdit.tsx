a73beeae97a7c0f60011b01dc3cdeb4e1126e5a9
chan-hwi <gazebo5@naver.com>
2023-11-17 18:42:47 +0900

    feat(flashcard): edit and delete features

---
+++ app/(pages)/(main)/flashcards/upload/[id]/FlashcardEdit.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { useCallback } from "react";
+import UploadWarningCard from "@/components/common/UploadWarningCard";
+import { useRouter } from "next/navigation";
+import * as z from "zod";
+import { useSession } from "next-auth/react";
+import FlashcardForm, {
+	FlashcardFormType,
+	flashcardFormSchema,
+} from "@/components/flashcards/FlashcardForm";
+import { usePatchFlashcard } from "@/lib/queries/flashcards";
+import { Flashcard } from "@/types/schema";
+
+interface FlashcardEditProps {
+	initialData: Flashcard;
+}
+
+function FlashcardEdit({ initialData }: FlashcardEditProps) {
+	const { status } = useSession();
+	const router = useRouter();
+	const patchFlashcard = usePatchFlashcard();
+
+	const formData: Partial<z.infer<typeof flashcardFormSchema>> = {
+		name: initialData.name,
+		description: initialData.description || "",
+		tags: initialData.Tags.map((tag) => tag.name),
+		acl: initialData.acl,
+		contents: initialData.Contents.map((content) => ({
+			id: content.id,
+			question: content.question,
+			answer: content.answer,
+			difficulty: content.difficulty,
+		})),
+	};
+
+	const handleSubmit = useCallback(
+		(values: z.infer<typeof flashcardFormSchema>, form: FlashcardFormType) => {
+			if (status !== "authenticated") {
+				alert("Please login");
+				return;
+			}
+			patchFlashcard.mutate(
+				{
+					id: initialData.id,
+					...values,
+				},
+				{
+					onSuccess(data, variables, context) {
+						form.reset();
+						router.push(`/flashcards/${data.data.id}`);
+					},
+					onError: (err) => {
+						console.log(err);
+						alert("문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
+					},
+				}
+			);
+		},
+		[patchFlashcard]
+	);
+
+	return (
+		<main className="container flex flex-col my-8 gap-8">
+			<UploadWarningCard />
+			<FlashcardForm onSubmit={handleSubmit} initialData={formData} />
+		</main>
+	);
+}
+
+export default FlashcardEdit;
---
+++ app/(pages)/(main)/flashcards/upload/[id]/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import FlashcardEdit from "./FlashcardEdit";
+import { db } from "@/lib/db";
+import { Flashcard } from "@/types/schema";
+
+interface Props {
+	params: {
+		id: string;
+	};
+}
+
+async function FlashcardEditPage(props: Props) {
+	const flashcard = (await db.flashcard.findUnique({
+		where: { id: parseInt(props.params.id) },
+		include: {
+			User: true,
+			Tags: true,
+			Contents: { orderBy: { order: "asc" } },
+		},
+	})) as Flashcard;
+
+	return <FlashcardEdit initialData={flashcard} />;
+}
+
+export default FlashcardEditPage;
