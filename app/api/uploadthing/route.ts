1342ebf818fe4dd7b728db4aab7101232c529dc8
chan-hwi <gazebo5@naver.com>
2023-12-03 21:01:06 +0900

    fix: uploadthing

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -5,4 +5,9 @@ import { imageSingleRouter } from "./core";
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
 	router: imageSingleRouter,
+	config: {
+		uploadthingId: process.env.UPLOADTHING_APP_ID,
+		uploadthingSecret: process.env.UPLOADTHING_SECRET,
+		callbackUrl: `${process.env.UPLOADTHING_URL}/api/uploadthing`,
+	}
 });
